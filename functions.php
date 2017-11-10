a1bea0a98adc8bad7d203aa71798297bd7abbf45
Johan Lund <johanjohan.lund@gmail.com>
2017-11-10 10:59:17 +0100

    exercises finished

--- functions.php
+++ functions.php
@@ -1,8 +1,32 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // "HTTP/1.0 404 Not Found"
-function status_header($code = 200) {}
+function status_header($code = 200) {
+	$status_code = [
+		200 => 'fuck you buddy!!!!!!!!',
+		500 => 'server melldor',
+	];
+	header('HTTP/1.1 ' . $code . " " . $status_code[$code]);
+}
+status_header(500);
 
-// [header => värde]
-function headers(array $headers = []) {}
+ //[header => värde];
+ //Connection: Upgrade;
+function headers(array $headers = []) {
+    foreach ($headers as $header => $value) {
+        header("$header: $value");
+    }
+}
+headers([
+    "Connection" => "Upgrade"
+]);
 
-function redirect($url, $code = 302) {}
+function redirect($url, $code = 302) {
+
+    header('Location:' . $url);
+
+}
+
+redirect('www.google.com');
---
+++ ovning2.php
@@ -0,0 +1,13 @@
+<?php require "functions.php"?>
+<html>
+	<body>
+		<form action="ovning2.php" method="post">
+			<input type="text" name="username" placeholder="Användarnamn"></input><br/>
+			<input type="text" name="email" placeholder="email"></input><br/>
+			<input type="text" name="birthdate" placeholder="birthdate" value="1983-12-19"></input><br/>
+			<input type="text" name="color" placeholder="color"></input><br/>
+			<button type="submit">Submit</button>
+		</form>
+		
+	</body>
+</html>
